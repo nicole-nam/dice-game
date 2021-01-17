cb22826ebcc38f9bf730437f607fb6247b74db03
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-01-17 15:25:57 -0500

    add switch player function

--- starter/script.js
+++ starter/script.js
@@ -10,11 +10,16 @@ const score0 = document.getElementById('score--0');
 const score1 = document.getElementById('score--1');
 const current0 = document.getElementById('current--0');
 const current1 = document.getElementById('current--1');
+//players
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+
 //dice
 const dice = document.querySelector('.dice');
 //score
 let score = 0;
 let currentScore = 0;
+let currentPlayer = 0;
 
 ////////////RESET THE GAME
 const init = () => {
@@ -27,6 +32,19 @@ const init = () => {
 
 init();
 
+////////////SWITCH PLAYERS
+const switchPlayer = () => {
+  // 1. remove class player--active and add it to player 2 vice versa
+  player0.classList.toggle('player--active');
+  player1.classList.toggle('player--active');
+  // 2. currentScore = 0 , of the current player
+  currentScore = 0;
+  document.getElementById(
+    `current--${currentPlayer}`
+  ).textContent = currentScore;
+  currentPlayer = currentPlayer === 0 ? 1 : 0;
+};
+
 ////////////ROLL DICE
 btnRoll.addEventListener('click', () => {
   // 1. generate random number
@@ -36,4 +54,13 @@ btnRoll.addEventListener('click', () => {
   dice.classList.remove('hidden');
   dice.src = `dice-${randomNum}.png`;
   // 3. add random number to the current number and show it
+  // if randomNum is 1 switch player
+  if (randomNum !== 1) {
+    currentScore += randomNum;
+    document.getElementById(
+      `current--${currentPlayer}`
+    ).textContent = currentScore;
+  } else {
+    switchPlayer();
+  }
 });
