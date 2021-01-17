17e6d48838d7d7b12ce369f4da5f32c97006b0fa
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-01-17 12:06:55 -0500

    add variables for selecting button elements

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//SELECTING ELEMENTS
+
+//buttons
+const btnNew = document.querySelector('.btn--new');
+console.log(btnNew);
+const btnRoll = document.querySelector('.btn--roll');
+console.log(btnNew);
+const btnHold = document.querySelector('.btn--hold');
+console.log(btnNew);
