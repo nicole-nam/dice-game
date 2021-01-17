cdf2c5c86bdce67737f2373544e595320f6b4f16
Eunjung Nam <eunjungnam@eunjungs-air.lan>
2021-01-17 12:22:29 -0500

    add hidden  class to dice element and reset scores to zero in init function

--- starter/script.js
+++ starter/script.js
@@ -15,3 +15,17 @@ const current1 = document.getElementById('current--1');
 
 //dice
 const dice = document.querySelector('.dice');
+
+let score = 0;
+
+//RESET THE GAME
+
+const init = () => {
+  // 1. remove the dice image
+  dice.classList.add('hidden');
+  // 2. set all scores to 0
+  score0.textContent = score;
+  score1.textContent = score;
+};
+
+init();
